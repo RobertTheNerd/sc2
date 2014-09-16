bf5bbf44209d4587a9b99fffcc4677a831b811d9
RobertTheNerd <robert.q.wang@gmail.com>
2014-09-16 11:47:22 -0700

    Update maven-scripts.sh

--- maven-scripts.sh
+++ maven-scripts.sh
@@ -2,3 +2,6 @@
 # http://stackoverflow.com/questions/778933/log4j-configuration-via-jvm-arguments
 
 mvn exec:java -Dconfig="file:/pathtoconfigfile" -Dexec.args="fullupdate"  -Dlog4j.configuration="/Users/robert/data/SolrUpdater/log4j.properties"
+
+
+mvn exec:java -Dconfig="file:/srv/sc2geeks/app/solrUpdater/config/config.xml" -Dexec.args="fullupdate"  -Dlog4j.configuration="file:/srv/sc2geeks/app/solrUpdater/log4j.properties"
